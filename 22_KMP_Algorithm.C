778b9344f73a177ad6d4c705e6c4eef8cd147fbe
Pranav Pawar <ppawar2612@gmail.com>
2024-05-13 00:57:26 +0530

    Update 22_KMP_Algorithm.C

--- 22_KMP_Algorithm.C
+++ 22_KMP_Algorithm.C
@@ -4,7 +4,6 @@
 void KMP_prefix(char* P, int m, int* pi) {
     int i = 1, j = 0;
     pi[0] = -1;
-
     while (i < m) {
         while (j >= 0 && P[i] != P[j]) {
             j = pi[j];
@@ -14,7 +13,6 @@ void KMP_prefix(char* P, int m, int* pi) {
         pi[i] = j;
     }
 }
-
 void KMP_match(char* P, char* T, int m, int n, int* pi) {
     int i = 0, j = 0;
     while (i < n) {
@@ -23,23 +21,29 @@ void KMP_match(char* P, char* T, int m, int n, int* pi) {
         }
         i++;
         j++;
+
         if (j == m) {
             printf("Pattern found at index %d\n", i - j);
-            j = pi[j-1];
+            j = pi[j];
         }
     }
-    printf("Pattern Ended");
+
+    if (j == m) {
+        printf("Pattern found at index %d\n", i - j);
+    } else {
+        printf("Pattern not found in text/Pattern Ended\n");
+    }
 }
 
-int main() {
+void main() {
     char text[] = "ABABDABACDABABCABAB";
     char pattern[] = "AB";
     int m = strlen(pattern);
     int n = strlen(text);
-    int pi[m + 1];
-
+    int *pi = (int *)malloc((m + 1) * sizeof(int));
+    clrscr();
     KMP_prefix(pattern, m, pi);
     KMP_match(pattern, text, m, n, pi);
-
-    return 0;
+    free(pi);
+    getch();
 }
