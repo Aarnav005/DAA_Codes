2604dfa592ddecad7ff2e00cb5bf1cc7c4c0b7e5
Pranav Pawar <ppawar2612@gmail.com>
2024-05-12 23:38:19 +0530

    Update 10_MatrixChainMultiplication.C

--- 10_MatrixChainMultiplication.C
+++ 10_MatrixChainMultiplication.C
@@ -1,66 +1,63 @@
-#include <stdio.h>
-#include <limits.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_SIZE 100
+int memo[10][10][2];
 
-void printOptimalParenthesis(int s[][MAX_SIZE], int i, int j); // Forward declaration
-
-void matrixChainOrder(int p[], int n) {
-    int m[MAX_SIZE][MAX_SIZE]; // m[i][j] stores minimum number of scalar multiplications needed to compute the matrix A[i]A[i+1]...A[j]
-    int s[MAX_SIZE][MAX_SIZE]; // s[i][j] stores the index of the matrix after which the product is split
-
-    // Initialize m[i][i] to 0 as it costs nothing to multiply one matrix
-    for (int i = 1; i <= n; i++)
-        m[i][i] = 0;
-
-    // Calculate m[i][j] and s[i][j] for all possible chain lengths l
-    for (int l = 2; l <= n; l++) {
-        for (int i = 1; i <= n - l + 1; i++) {
-            int j = i + l - 1;
-            m[i][j] = INT_MAX;
-
-            for (int k = i; k < j; k++) {
-                int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                if (q < m[i][j]) {
-                    m[i][j] = q;
-                    s[i][j] = k;
-                }
-            }
-        }
-    }
-
-    // Print the minimum number of scalar multiplications
-    printf("Minimum number of scalar multiplications: %d\n", m[1][n]);
-
-    // Print the optimal parenthesization
-    printf("Optimal parenthesization: ");
-    printOptimalParenthesis(s, 1, n);
-    printf("\n");
-}
-
-void printOptimalParenthesis(int s[][MAX_SIZE], int i, int j) {
+int MatrixChainOrder(int p[], int i, int j){
+    int k;
+    int min = 9999;
+    int count;
+    int k1;
     if (i == j)
-        printf("A%d", i);
-    else {
-        printf("(");
-        printOptimalParenthesis(s, i, s[i][j]);
-        printOptimalParenthesis(s, s[i][j] + 1, j);
-        printf(")");
+	return 0;
+    if(memo[i][j][0] != 0){
+	return memo[i][j][1];
     }
+    for (k = i; k < j; k++)
+    {
+	count = MatrixChainOrder(p, i, k) + MatrixChainOrder(p, k + 1, j) + p[i - 1] * p[k] * p[j];
+
+	if (count < min){
+	    min = count;
+	    k1 = k;
+	}
+    }
+    memo[i][j][0] = k1;
+    memo[i][j][1] = min;
+    return min;
 }
 
-int main() {
-    int n;
-    printf("Enter the number of matrices: ");
-    scanf("%d", &n);
-
-    int p[MAX_SIZE]; // Array to store dimensions of matrices
-    printf("Enter the dimensions of matrices (including dimensions of result matrix): ");
-    for (int i = 0; i <= n; i++) {
-        scanf("%d", &p[i]);
+void main(){
+    int n = 4,k,i,j;
+    int cost[] = {5,4,2,3,4};
+    int M[4][4] = {0};
+    clrscr();
+    for(i=0;i<10;i++){
+	for(j=0;j<n;j++){
+	    memo[i][j][0] = 0;
+	    memo[i][j][1] = 0;
+	}
     }
-
-    matrixChainOrder(p, n);
-
-    return 0;
+    for(i=0;i<n;i++){
+	for(j=0;j<n;j++){
+	    if(j>=i){
+		printf("%d ", MatrixChainOrder(cost, i+1, j+1));
+	    }else{
+		printf("0 ");
+	    }
+	}
+	printf("\n");
+    }
+    printf("\n");printf("\n");
+    for(i=0;i<n;i++){
+	for(j=0;j<n;j++){
+	    if(j>=i){
+		printf("%d ", memo[i+1][j+1][0]);
+	    }else{
+		printf("0 ");
+	    }
+	}
+	printf("\n");
+    }
+    getch();
 }
