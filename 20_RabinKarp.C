cc36a90d85ef2b83ac55c6dbfb84b22174bd105b
Pranav Pawar <ppawar2612@gmail.com>
2024-05-13 01:20:50 +0530

    Update 20_RabinKarp.C

--- 20_RabinKarp.C
+++ 20_RabinKarp.C
@@ -1,78 +1,52 @@
-
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
-#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
 #define d 256
+#define q 13
+int sp = 0;
+void rabin_karp(char *text, char *pattern) {
+    int n = strlen(text);
+    int m = strlen(pattern);
+    int p=0, t=0, h=1, j;
+    int i;
 
-int hashToDigit(long long int hash, int q)
-{
-    return hash % q;
+    for(i=1 ; i<=m-1 ; i++)
+        h = (h * d) % q;
+
+    for(i=0 ; i<m ; i++) {
+        t = (d * t + text[i]) % q;
+        p = (d * p + pattern[i]) % q;
+    }
+
+    for(i=0 ; i<=n-m ; i++) {
+        if(p == t) {
+            for(j=0 ; j<m ; j++) {
+                if(text[i+j] != pattern[j]) 
+                    break;
+            }
+            if(j == m)
+                printf("\nPattern found at index %d", i);
+            else 
+                sp++;
+        }
+        if(i < n-m) {
+            t = (d * (t - text[i] * h) + text[i+m]) % q;
+            if(t < 0)
+                t = (t + q) % q;
+        }
+    }
+    printf("\nSpurious Hits: %d", sp);
 }
 
-void search(char *pattern, char *text, int q)
-{
-    int M = strlen(pattern);
-    int N = strlen(text);
-    int i, j;
-    int p = 0;
-    int t = 0;
-    int h = 1;
-    int spuriousHits = 0;
-    int patternCount = 0;
-    for (i = 0; i < M - 1; i++)
-	h = (h * d) % q;
-    for (i = 0; i < M; i++)
-    {
-	p = (d * p + pattern[i]) % q;
-	t = (d * t + text[i]) % q;
-    }
-    for (i = 0; i <= N - M; i++)
-    {
-	if (p == t)
-	{
-	    for (j = 0; j < M; j++)
-	    {
-		if (text[i + j] != pattern[j])
-		    break;
-	    }
-	    if (j == M)
-	    {
-		printf("Pattern found at index %d\n", i); patternCount++;
-	    }
-	    else
-	    {
-		printf("Spurious hit at index %d\n", i); spuriousHits++;
-	    }
-	}
-
-	if (i < N - M)
-	{
-	    t = (d * (t - text[i] * h) + text[i + M]) % q;
-	    if (t < 0)
-		t = (t + q);
-	}
-    }
-    printf("Number of spurious hits: %d\n", spuriousHits);
-    printf("Number of pattern occurrences found: %d\n", patternCount);
-}
-
-int main()
-{
-    char text[100];
-    char pattern[100];
-    int q;
-    int patternHashDigit;
+void main() {
+    char *text = (char *) malloc (MAX * sizeof(char));
+    char *pattern = (char *) malloc (MAX * sizeof(char));
     clrscr();
     printf("Enter the text: ");
-    scanf("%s", text);
+    scanf("%99s", text);
     printf("Enter the pattern: ");
-    scanf("%s", pattern);
-    printf("Enter the value of q: ");
-    scanf("%d", &q);
-    patternHashDigit = hashToDigit(123456789, q);
-    printf("Hash value of pattern converted into digit: %d\n", patternHashDigit);
-    search(pattern, text, q);
+    scanf("%99s", pattern);
+    rabin_karp(text, pattern);
     getch();
-    return(0);
 }
