93f2a04abbd660773bc82eedc5f2cd89f99e89f9
Pranav Pawar <ppawar2612@gmail.com>
2024-05-12 15:06:23 +0530

    Update 02_StrassenMatrixMultiplication.C

--- 02_StrassenMatrixMultiplication.C
+++ 02_StrassenMatrixMultiplication.C
@@ -1,65 +1,66 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-int arr1[] = {1,0,0,1};
-int arr2[] = {1,2,3,4};
-int ans[4];
+#include <stdio.h>
+#include <stdlib.h>
 
-void stressons(int X[], int Y[])
-{
-	int A, B, C, D, E, F, G, H, P1, P2, P3, P4, P5, P6, P7;
-	A = X[0];
-	B = X[1];
-	C = X[2];
-	D = X[3];
-	E = Y[0];
-	F = Y[1];
-	G = Y[2];
-	H = Y[3];
-	P1 = A*(F-H);
-	P2 = H*(A+B);
-	P3 = E*(C+D);
-	P4 = D*(G-E);
-	P5 = (A+D)*(E+H);
-	P6 = (B-D)*(G+H);
-	P7 = (A-C)*(E+F);
-	ans[0] = P4+P5+P6-P2;
-	ans[1] = P1+P2;
-	ans[2] = P3+P4;
-	ans[3] = P1-P3+P5-P7;
-}
-void multiplication(int A[],int B[]){
-	int i,j,k;
-	for(i=0;i<2;i++){
-		for(j=0;j<2;j++){
-			ans[i * 2 + j] = 0;
-			for(k=0;k<2;k++){
-				ans[i * 2 + j] += A[i*2+k]*B[k*2+j];
-			}
+void main() {
+    int rows1, cols1;
+    int i,j;
+    int m1, m2, m3, m4, m5, m6, m7, p1, p2, p3, p4;
+    int **matrix1 = (int**) malloc (rows1 * sizeof(int*));
+    int **matrix2 = (int**) malloc (rows1 * sizeof(int*));
+    int **matrix3 = (int**) malloc (rows1 * sizeof(int*));
+    clrscr();
+    printf("Enter rows and columns for the matrix: ");
+    scanf("%d %d", &rows1, &cols1);
+    for(i = 0 ; i < rows1 ; i++) {
+		matrix1[i] = (int*) malloc (cols1 * sizeof(int));
+		if(matrix1[i] == NULL) {
+			printf("Memory Allocation failed");
+			return -1;
 		}
-	}
+    }
+    for(i = 0 ; i < rows1 ; i++) {
+		matrix2[i] = (int*) malloc (cols1 * sizeof(int));
+		if(matrix2[i] == NULL) {
+			printf("Memory Allocation failed");
+			return -1;
+		}
+    }
+    for(i = 0 ; i < rows1 ; i++) {
+		matrix3[i] = (int*) malloc (cols1 * sizeof(int));
+		if(matrix3[i] == NULL) {
+			printf("Memory Allocation failed");
+			return -1;
+		}
+    }
+    printf("\nEnter the first matrix:");
+    for(i = 0 ; i < rows1 ; i++) {
+		for(j = 0 ; j < cols1 ; j++) {
+			scanf("%d", &matrix1[i][j]);
+		}
+    }
+    printf("\nEnter the second matrix:");
+    for(i = 0 ; i < rows1 ; i++) {
+		for(j = 0 ; j < cols1 ; j++) {
+			scanf("%d", &matrix2[i][j]);
+		}
+    }
+    m1 = matrix1[0][0] * (matrix2[0][1] - matrix2[1][1]);
+    m2 = (matrix1[0][0] + matrix1[0][1]) * matrix2[1][1];
+    m3 = (matrix1[1][0] + matrix1[1][1]) * matrix2[0][0];
+    m4 = matrix1[1][1] * (matrix2[1][0] - matrix2[0][0]);
+    m5 = (matrix1[0][0] + matrix1[1][1]) * (matrix2[0][0] + matrix2[1][1]);
+    m6 = (matrix1[0][1] - matrix1[1][1]) * (matrix2[1][0] + matrix2[1][1]);
+    m7 = (matrix1[0][0] - matrix1[1][0]) * (matrix2[0][0] + matrix2[0][1]);
+    matrix3[0][0] = m5 + m4 - m2 + m6;
+    matrix3[0][1] = m1 + m2;
+    matrix3[1][0] = m3 + m4;
+    matrix3[1][1] = m1 + m5 - m3 - m7;
+    printf("\nMatrix Multiplication:\n");
+    for(i = 0 ; i < rows1 ; i++) {
+		for(j = 0 ; j < cols1 ; j++) {
+			printf("%d ", matrix3[i][j]);
+		}
+		printf("\n");
+    }
+    getch();
 }
-
-void printmat(char wat,int mat[]){
-	int i;
-	printf("The array %c is: \n",wat);
-	for(i=0;i<4;i++){
-		printf("%d ",mat[i]);
-		if(i==1)
-			printf("\n");
-	}
-	printf("\n");
-	printf("\n");
-}
-
-int main(){
-	clrscr();
-	printmat('A',arr1);
-	printmat('B',arr2);
-	stressons(arr1,arr2);
-	printf("The answer is: ");
-	printmat('C',ans);
-	getch();
-	return 0;
-}
- 
