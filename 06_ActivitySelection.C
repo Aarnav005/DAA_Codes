ec142cdd1090ef5af61e794f489b30a7fbac5f5a
Pranav Pawar <ppawar2612@gmail.com>
2024-05-12 21:33:03 +0530

    Update 06_ActivitySelection.C

--- 06_ActivitySelection.C
+++ 06_ActivitySelection.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<conio.h>
 #include <stdlib.h>
 
 typedef struct {
@@ -13,13 +14,12 @@ int compare(const void* a, const void* b) {
 }
 
 void activitySelection(Activity activities[], int n) {
+    int j=0;
+    int i;
     qsort(activities, n, sizeof(Activity), compare);
-
     printf("Selected activities:\n");
-    int j = 0;
     printf("(%d, %d)\n", activities[j].start, activities[j].finish);
-
-    for (int i = 1; i < n; i++) {
+    for (i = 1; i < n; i++) {
         if (activities[i].start >= activities[j].finish) {
             printf("(%d, %d)\n", activities[i].start, activities[i].finish);
             j = i;
@@ -27,21 +27,17 @@ void activitySelection(Activity activities[], int n) {
     }
 }
 
-int main() {
-    int n;
+void main() {
+    int n,i;
+    Activity* activities = (Activity*)malloc(n * sizeof(Activity));
     printf("Enter the number of activities: ");
     scanf("%d", &n);
-
-    Activity* activities = (Activity*)malloc(n * sizeof(Activity));
-
     printf("Enter the start and finish times of the activities:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d %d", &activities[i].start, &activities[i].finish);
+    for (i = 0; i < n; i++) {
+	    scanf("%d %d", &activities[i].start, &activities[i].finish);
     }
-
     activitySelection(activities, n);
-    free(activities);
-    return 0;
+    getch();
 }
 
 /*
